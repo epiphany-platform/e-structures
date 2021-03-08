b98df141e425be20c9917d93f7f29ab34db93619
Mateusz Kyc <mateusz.kyc@gmail.com>
2021-03-08 16:39:35 +0100

    add save and load functions for azbi and azks packages

--- utils/load/load.go
+++ utils/load/load.go
@@ -1,6 +1,8 @@
 package load
 
 import (
+	azbi "github.com/epiphany-platform/e-structures/azbi/v0"
+	azks "github.com/epiphany-platform/e-structures/azks/v0"
 	hi "github.com/epiphany-platform/e-structures/hi/v0"
 	"io/ioutil"
 	"os"
@@ -8,6 +10,7 @@ import (
 	st "github.com/epiphany-platform/e-structures/state/v0"
 )
 
+// TODO handle problematic situation when some of config structures are "almost" empty and validation fails (https://github.com/epiphany-platform/e-structures/issues/10)
 func State(path string) (*st.State, error) {
 	if _, err := os.Stat(path); os.IsNotExist(err) {
 		return st.NewState(), nil
@@ -22,6 +25,40 @@ func State(path string) (*st.State, error) {
 			return nil, err
 		}
 		return state, nil
+	}
+}
+
+func AzBIConfig(path string) (*azbi.Config, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return azbi.NewConfig(), nil
+	} else {
+		config := &azbi.Config{}
+		bytes, err := ioutil.ReadFile(path)
+		if err != nil {
+			return nil, err
+		}
+		err = config.Unmarshal(bytes)
+		if err != nil {
+			return nil, err
+		}
+		return config, nil
+	}
+}
+
+func AzKSConfig(path string) (*azks.Config, error) {
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+		return azks.NewConfig(), nil
+	} else {
+		config := &azks.Config{}
+		bytes, err := ioutil.ReadFile(path)
+		if err != nil {
+			return nil, err
+		}
+		err = config.Unmarshal(bytes)
+		if err != nil {
+			return nil, err
+		}
+		return config, nil
 	}
 }
 
--- utils/save/save.go
+++ utils/save/save.go
@@ -1,6 +1,8 @@
 package save
 
 import (
+	azbi "github.com/epiphany-platform/e-structures/azbi/v0"
+	azks "github.com/epiphany-platform/e-structures/azks/v0"
 	hi "github.com/epiphany-platform/e-structures/hi/v0"
 	st "github.com/epiphany-platform/e-structures/state/v0"
 	"io/ioutil"
@@ -8,6 +10,30 @@ import (
 
 func State(path string, state *st.State) error {
 	bytes, err := state.Marshal()
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(path, bytes, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func AzBIConfig(path string, config *azbi.Config) error {
+	bytes, err := config.Marshal()
+	if err != nil {
+		return err
+	}
+	err = ioutil.WriteFile(path, bytes, 0644)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func AzKSConfig(path string, config *azks.Config) error {
+	bytes, err := config.Marshal()
 	if err != nil {
 		return err
 	}
